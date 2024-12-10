dabefc29022885a0433d1e28d1ad0a3dc5237f02
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-12-10 19:51:39 +0200

    Updated package.json file and scss files.

--- restrict-kb-by-user-role.php
+++ restrict-kb-by-user-role.php
@@ -4,7 +4,7 @@
  * Plugin Name:   Restrict KB Access by User Role - Knowledgebase Addon
  * Plugin URI:      https://1.envato.market/bkbm-wp
  * Description:     Restrict KB Addon allows you to limit access of KB content according to user role. 
- * Version:           1.1.0
+ * Version:           1.1.1
  * Author:            Mahbub Alam Khan
  * Author URI:      https://bluewindlab.net
  * Text Domain:    bkb_rkb
@@ -21,7 +21,7 @@ define('BKBRKB_PARENT_PLUGIN_INSTALLED_VERSION', get_option('bwl_kb_plugin_versi
 define('BKBRKB_ADDON_PARENT_PLUGIN_TITLE', 'BWL Knowledge Base Manager Plugin');
 define('BKBRKB_ADDON_TITLE', 'Restrict KB Access by User Role');
 define('BKBRKB_PARENT_PLUGIN_REQUIRED_VERSION', '1.4.2'); // change plugin required version in here.
-define('BKBRKB_ADDON_CURRENT_VERSION', '1.1.0'); // change plugin current version in here.
+define('BKBRKB_ADDON_CURRENT_VERSION', '1.1.1'); // change plugin current version in here.
 define('BKBRKB_ADDON_INSTALLATION_TAG', 'bkbm_rkbur_installation_' . str_replace('.', '_', BKBRKB_ADDON_CURRENT_VERSION));
 define('BKBRKB_ADDON_UPDATER_SLUG', plugin_basename(__FILE__)); // change plugin current version in here.
 define("BKBRKB_ADDON_CC_ID", "13722991"); // Plugin codecanyon Id.
